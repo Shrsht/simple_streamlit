d245cdd47d82133138883dbf4673e055505c8c3d
Shrsht <102553723+Shrsht@users.noreply.github.com>
2024-04-23 14:54:22 -0400

    Update model.py

--- model.py
+++ model.py
@@ -2,10 +2,24 @@ from keybert import KeyBERT
 
 
 kb = KeyBERT()
-keywords = kb.extract_keywords(resume_text, stop_words = 'english',
+keywords_1 = kb.extract_keywords(resume_text, stop_words = 'english',
+                               keyphrase_ngram_range = (1,1),
+                               nr_candidates= 0.2*len(resume_text),
+                               use_mmr = True,
+                               top_n = 20,
+                               diversity = 0.5,)
+keywords_2 = kb.extract_keywords(resume_text, stop_words = 'english',
+                               keyphrase_ngram_range = (1,2),
+                               nr_candidates= 0.2*len(resume_text),
+                               use_mmr = True,
+                               top_n = 20,
+                               diversity = 0.5,)
+keywords_3 = kb.extract_keywords(resume_text, stop_words = 'english',
                                keyphrase_ngram_range = (1,3),
                                nr_candidates= 0.2*len(resume_text),
                                use_mmr = True,
                                top_n = 20,
-                               diversity = 0.8,)
+                               diversity = 0.5,)
+
+
 
