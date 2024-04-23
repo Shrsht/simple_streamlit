3d813722d82dd1ef771c8b39a179706e2caafdf5
Shrsht <102553723+Shrsht@users.noreply.github.com>
2024-04-23 15:21:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,9 @@ df_portals= get_data()
 st.write(
 
     '''
-    ### This App is designed to give you a breakdown of the various keywords in your resume as well as give you a recommendation
-    of a few jobs you would be a good fit for.
+    ### This App is designed to give you a breakdown of the various keywords in your resume as well as give you a recommendation of a few jobs you would be a good fit for.
 
-    To learn about your Resume, please upload it below:
+    ## To learn about your Resume, please upload it below:
 
     '''
 
@@ -94,26 +93,62 @@ if resume_file:
   # Display the image in the first column
     col1.image(page_image)
   
-    col2.text_area("Page Text", height=800, value= page_text,
+    col2.text_area( height=800, value= page_text,
                   key="my_text_area", on_change=on_text_area_change)
+
+    st.write(
+        '''
+        ## Thank you for uploading your resume! The most relevant keywords we found in your resume are as follows:
+
+        '''
+    )
+
+   
+    st.write (
+        '''
+        ### One-Letter Keywords:
+        ''' )
     
-    keywords = kb.extract_keywords(page_text, stop_words = 'english',
+    keywords_1 = kb.extract_keywords(page_text, stop_words = 'english',
+                               keyphrase_ngram_range = (1,1),
+                               nr_candidates= 0.2*len(page_text),
+                               use_mmr = True,
+                               top_n = 20,
+                               diversity = 0.8)
+    for tup in keywords_1[0:5]:
+        st.caption(f':blue[tup[0]]') 
+
+
+    st.write (
+        '''
+        ### Two-Letter Keywords:
+        ''' )
+
+    keywords_2 = kb.extract_keywords(page_text, stop_words = 'english',
+                               keyphrase_ngram_range = (1,2),
+                               nr_candidates= 0.2*len(page_text),
+                               use_mmr = True,
+                               top_n = 20,
+                               diversity = 0.8)
+    
+    for tup in keywords_2[0:5]:
+        st.caption(f':blue[tup[0]]') 
+
+
+    st.write (
+        '''
+        ### Three-Letter Keywords:
+        ''' )
+
+    keywords_3 = kb.extract_keywords(page_text, stop_words = 'english',
                                keyphrase_ngram_range = (1,3),
                                nr_candidates= 0.2*len(page_text),
                                use_mmr = True,
                                top_n = 20,
-                               diversity = 0.8,)
-    scores = []
-    words = []
+                               diversity = 0.8)
     
-    for i in keywords:
-        words.append(i[0])
-        scores.append(i[1])
-
-    
-    st.write("Your Resume contains the following keywords:")
-
-    st.write(words)
+    for tup in keywords_2[0:5]:
+        st.caption(f':blue[tup[0]]') 
 
 
 
@@ -121,42 +156,20 @@ if resume_file:
 # PART 2 : Plot
 # ------------------------------
 
-    st.write(
-        '''
-        ## Issues with the Model:
-
-        Model is able to give us a breakdown of keywords in our inputted resume. 
-        However it is still unable to recognize strings like '__________'. We need ot work on cleaning the resumes as they are uploaded
-        The model still can't recognize which entities represent what keywords 
-        i.e We want to know if a specific keyword is a 'Job Description' or a 'Skill' , etc.
-        
-
-       ##  Way Forward:
-
-        We need to improve our model using NER (Named Entity Recognition) models that can recognize the exact label of a given resume. We can then match these
-        named-entities with job descriptions in our dataframe. 
-
-        Such NER models are available and can be included in the model.py file. 
-        Furthermore we need to include a section where we clean the resume text and remove values like '______'.
-        Our app can also have better visualisations in terms of the graphs and keywords we collect. 
-
-        ''')
 
     st.write(
     '''
-    ## Visualize
-    Plotting the frequency of Job Portals in our Dataset
+    ## Given your keywords, let us have a look at what jobs match your resume: 
     '''
     )
 
 
 
-# fig = fig = go.Figure(data=[go.Bar(x=df_portals['Job Portal'].value_counts().index, 
-#                                    y= df_portals['Job Portal'].value_counts().values)])
 
-    fig = fig = go.Figure(data=[go.Bar(x = words, 
+
+    #fig = go.Figure(data=[go.Bar(x = words, 
                                     y = scores)])
 
-    st.plotly_chart(fig)
+    #st.plotly_chart(fig)
 
 
